f776977b2072fc420ece9d3d9440b0349c9efba8
Ziad Hussain <zhussain@Ziad-Hussains-MacBook-Pro.local>
2010-08-15 05:20:26 -0500

    real chat

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,6 @@
 class Stream < ActiveRecord::Base
   SITEDOMAIN = "videowell.heroku.com"
   PAYPALBUTTON = "NEFKGHHS9QMKJ"
+  DEVKEY = "d55ace697f7aa67a0dac574e-64990ec57f23"
   belongs_to :user
 end
